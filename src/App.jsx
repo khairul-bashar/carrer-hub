8b1a5a5e9cb37541dbf259e7403583207d7f3ffa
khairul-bashar <khairulbashar5424@gmail.com>
2023-06-25 07:56:25 +0600

    starter pack install

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-5xl font-bold text-center'>career hub</h1>
+    </>
+  )
+}
+
+export default App
